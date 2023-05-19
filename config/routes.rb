81bc53e94bbc13424936a38063decc2e17270373
Ymow Wu <kenshin0812@hotmail.com>
2023-05-19 12:36:24 +0800

    Inintial project
     - start from rails 7
     - integrate hotwire and stimulus
     - Real Time Notification System
     - To do List with Hotwire and Stimulus

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/create'
+  resources :tasks
+  post "tasks/:id/toggle", to: "tasks#toggle"
   resources :notifications, only: [ :index ]
   resources :posts
+  resources :tasks
   devise_for :users
   root "posts#index"
+
 end
